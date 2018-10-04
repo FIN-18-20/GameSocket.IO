0c2ed9140418086c33732fea834ad7aabdfddc96
Kalaneee <kalanehd@gmail.com>
2018-10-04 22:10:03 +0200

    First commit

--- server.js
+++ server.js
@@ -20,36 +20,38 @@ app.set('port', (process.env.PORT || 5000))
 
 // Routing
 app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, 'index.html'));
+	response.sendFile(path.join(__dirname, 'index.html'));
 });
 
 server.listen(app.get('port'), function() {
-  console.log('Starting server on port 5000');
+	console.log('Starting server on port 5000');
 });
 
 var players = {};
 io.on('connection', function(socket) {
-  socket.on('new player', function() {
-    players[socket.id] = {
-      x: 300,
-      y: 300
-    };
-  });
-  socket.on('movement', function(data) {
-    var player = players[socket.id] || {};
-    if (data.left) {
-      player.x -= 5;
-    }
-    if (data.up) {
-      player.y -= 5;
-    }
-    if (data.right) {
-      player.x += 5;
-    }
-    if (data.down) {
-      player.y += 5;
-    }
-  });
+
+	socket.on('new player', function() {
+		players[socket.id] = {
+			x: 300,
+			y: 300
+		};
+	});
+
+	socket.on('movement', function(data) {
+		var player = players[socket.id] || {};
+		if (data.left) {
+			player.x -= 5;
+		}
+		if (data.up) {
+			player.y -= 5;
+		}
+		if (data.right) {
+			player.x += 5;
+		}
+		if (data.down) {
+			player.y += 5;
+		}
+	});
 
 	socket.on('disconnect', function(data) {
 		console.log('Got disconnect!');
@@ -63,7 +65,6 @@ io.on('connection', function(socket) {
 });
 
 
-
 setInterval(function() {
-  io.sockets.emit('state', players);
+	io.sockets.emit('state', players);
 }, 1000 / 60);
--- static/game.js
+++ static/game.js
@@ -9,33 +9,33 @@ var movement = {
 document.addEventListener('keydown', function(event) {
   switch (event.keyCode) {
     case 65: // A
-      movement.left = true;
-      break;
+    movement.left = true;
+    break;
     case 87: // W
-      movement.up = true;
-      break;
+    movement.up = true;
+    break;
     case 68: // D
-      movement.right = true;
-      break;
+    movement.right = true;
+    break;
     case 83: // S
-      movement.down = true;
-      break;
+    movement.down = true;
+    break;
   }
 });
 document.addEventListener('keyup', function(event) {
   switch (event.keyCode) {
     case 65: // A
-      movement.left = false;
-      break;
+    movement.left = false;
+    break;
     case 87: // W
-      movement.up = false;
-      break;
+    movement.up = false;
+    break;
     case 68: // D
-      movement.right = false;
-      break;
+    movement.right = false;
+    break;
     case 83: // S
-      movement.down = false;
-      break;
+    movement.down = false;
+    break;
   }
 });
 
